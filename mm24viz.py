edcbcee8773c86c996d0bb5d434c66401d209955
jrhea815 <126609379+jrhea815@users.noreply.github.com>
2024-03-14 11:18:51 -0400

    Update mm24viz.py

--- mm24viz.py
+++ mm24viz.py
@@ -1,61 +1,42 @@
 import streamlit as st
 import pandas as pd
-st.set_page_config(layout="wide")
 
+st.set_page_config(layout="wide")
 
 def probability_viz_1():
     # Load data from a CSV file
     csv_file_path = "mm24predictions.csv"
     data = pd.read_csv(csv_file_path)  
 
-    # Page layout
-    page = st.sidebar.radio("Page", ["Probabilities", "Team Comparison"])
-
     # Sidebar filters
-    if page == "Probabilities":
-        min_prob, max_prob = st.sidebar.slider("Probability Range", 0.0, 1.0, (0.0, 1.0))
-    else:
-        team_filter = st.sidebar.selectbox("Select Team", data["Team1"].unique())
+    min_prob, max_prob = st.sidebar.slider("Probability Range", 0.0, 1.0, (0.0, 1.0))
 
     # Filter data
-    if page == "Probabilities":
-        filtered_data = data[(data["Probability"] >= min_prob) & (data["Probability"] <= max_prob)]
-    else:
-        filtered_data = data[data["Team1"] == team_filter]
+    filtered_data = data[(data["Probability"] >= min_prob) & (data["Probability"] <= max_prob)]
 
     # Display filtered data
     st.write(filtered_data)
 
-def probablitiy_viz_2():
-        # Load data from a CSV file
+def probability_viz_2():
+    # Load data from a CSV file
     csv_file_path = "mm-24-predictions.csv"
     data = pd.read_csv(csv_file_path)  
 
-    # Page layout
-    page = st.sidebar.radio("Page", ["Probabilities", "Team Comparison"])
-
     # Sidebar filters
-    if page == "Probabilities":
-        min_prob, max_prob = st.sidebar.slider("Probability Range", 0.0, 1.0, (0.0, 1.0))
-    else:
-        team_filter = st.sidebar.selectbox("Select Team", data["Team1"].unique())
+    min_prob, max_prob = st.sidebar.slider("Probability Range", 0.0, 1.0, (0.0, 1.0))
 
     # Filter data
-    if page == "Probabilities":
-        filtered_data = data[(data["Probability"] >= min_prob) & (data["Probability"] <= max_prob)]
-    else:
-        filtered_data = data[data["Team1"] == team_filter]
+    filtered_data = data[(data["Probability"] >= min_prob) & (data["Probability"] <= max_prob)]
 
     # Display filtered data
     st.write(filtered_data)
 
-
-
-
 if __name__ == "__main__":
-    page = st.sidebar.radio("Page", ["Probability Viz 1", "Probability Viz 2"])
+    page_1 = "NCAAB All Analysis"
+    page_2 = "NCAAB 23-24 Analysis"
+    page = st.sidebar.selectbox("Page", [page_1, page_2])
 
-    if page == "Probability Viz 1":
+    if page == page_1:
         probability_viz_1()
-    elif page == "Probability Viz 2":
+    elif page == page_2:
         probability_viz_2()
