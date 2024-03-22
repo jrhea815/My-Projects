e5901e61818c1714ee1fcf2cd74159b350566c4f
jrhea815 <126609379+jrhea815@users.noreply.github.com>
2024-03-22 10:27:07 -0400

    Update streamlit_predictor_viz.py

--- streamlit_predictor_viz.py
+++ streamlit_predictor_viz.py
@@ -43,12 +43,6 @@ if __name__ == "__main__":
     # Read the CSV file
     df = pd.read_csv(csvfile)
     
-    # Filter by probabilities
-    min_survive = st.slider('Minimum Survive Probability', min_value=0.0, max_value=100.0, value=0.0)
-    min_defeat = st.slider('Minimum Defeat Probability', min_value=0.0, max_value=100.0, value=0.0)
-    
-    filtered_df = df[(df['survive'] >= min_survive) & (df['defeat'] >= min_defeat)]
-    
     # Select team 1
     team_1 = st.selectbox('Select Team 1', options=filtered_df['team_name_1'].unique(), key="team_1")
     
